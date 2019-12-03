5688eb277602f8bee06077d73af5bb41c5a02fdc
jcsmith1860377 <55199762+jcsmith1860377@users.noreply.github.com>
2019-12-03 11:28:41 -0600

    Update dbptest.sql

--- dbptest.sql
+++ dbptest.sql
@@ -19,38 +19,46 @@ CREATE  TABLE COACHES(
     Title               Char(30) 		NOT NULL,
     Travel              Char(3) 		NOT NULL,
     CONSTRAINT          COACHES_PK  	PRIMARY KEY(CoachID)
-    CONSTRAINT          RECRUITS_HIGH_FK    FOREIGN KEY(HighSchoolID)
-                            REFERENCES HIGHSCHOOL(HighSchoolID)
+    CONSTRAINT          COACH_PLAYERS_FK    FOREIGN KEY(PlayerID)
+                            REFERENCES PLAYERS(PlayerID)
                                 ON UPDATE CASCADE
     );
 
 CREATE  TABLE HIGHSCHOOL(
     HighSchoolID        Int(3) 			NOT NULL,
     HighSchoolName      Char(50)     	NOT NULL,
+	PlayerID			Int(4)			NOT NULL,
     StreetAddress       Char(100)   	NOT NULL,
     City                Char(30)        NOT NULL,
     State               Char(2) 		NOT NULL,
     ZIP                 Int(5) 			NOT NULL,
     Distance            Int(5) 			NOT NULL,
     CONSTRAINT          HIGHSCHOOL_PK  	PRIMARY KEY(HighSchoolID)
+	CONSTRAINT          HIGH_PLAYERS_FK    FOREIGN KEY(PlayerID)
+                            REFERENCES PLAYERS(PlayerID)
+                                ON UPDATE CASCADE
     );
 
 CREATE  TABLE MEDICALSTAFF(
-    StaffID             Int(3) 		NOT NULL,
-    FirstName		Char(30)	NOT NULL,
+    StaffID             Int(3) 			NOT NULL,
+    FirstName			Char(30)		NOT NULL,
     LastName            Char(30)    	NOT NULL,
-    Title               Char(50) 	NOT NULL,
-    Travel              Char(1) 	NOT NULL,
-    CONSTRAINT          MEDICALSTAFF_PK	PRIMARY KEY(StaffID)
+	PlayerID			Int(4)			NOT NULL,
+    Title               Char(50) 		NOT NULL,
+    Travel              Char(1) 		NOT NULL,
+    CONSTRAINT          MEDICALSTAFF_PK		PRIMARY KEY(StaffID)
+	CONSTRAINT          MEDIC_PLAYERS_FK    FOREIGN KEY(PlayerID)
+                            REFERENCES PLAYERS(PlayerID)
+                                ON UPDATE CASCADE
     );
 
 CREATE  TABLE RECRUITS(
-    RecruitID           Int(3) 		NOT NULL,
-    HighSchoolID        Int(3) 		NOT NULL,
-    FirstName           Char(30)	NOT NULL,
-    LastName            Char(30)   	NOT NULL,
-    Position            Char(25) 	NOT NULL,
-    GradYear            Int(4) 		NOT NULL,
+    RecruitID           Int(3) 			NOT NULL,
+    HighSchoolID        Int(3) 			NOT NULL,
+    FirstName           Char(30)		NOT NULL,
+    LastName            Char(30)   		NOT NULL,
+    Position            Char(25) 		NOT NULL,
+    GradYear            Int(4) 			NOT NULL,
     CONSTRAINT          RECRUITS_PK         PRIMARY KEY(RecruitID),
     CONSTRAINT          RECRUITS_HIGH_FK    FOREIGN KEY(HighSchoolID)
                             REFERENCES HIGHSCHOOL(HighSchoolID)
@@ -58,11 +66,11 @@ CREATE  TABLE RECRUITS(
     );
 
 CREATE  TABLE PLAYEREQ(
-    PlayerEQID          Int(3) 		NOT NULL,
-    JerseyNumber        Int(2)     	NOT NULL,
+    PlayerEQID          Int(3) 			NOT NULL,
+    PlayerID        	Int(4)     		NOT NULL,
     Description         Char(100)   	NOT NULL,
-    Travel              Char(1)    	NOT NULL,
-    ExpectedLife        Int(2) 		NOT NULL,
+    Travel              Char(1)    		NOT NULL,
+    ExpectedLife        Int(2) 			NOT NULL,
     CONSTRAINT          PLAYEREQ_PK            	PRIMARY KEY(PlayerEQID),
     CONSTRAINT          PLAYEREQ_PLAYERS_FK     FOREIGN KEY(JerseyNumber)
                             REFERENCES PLAYERS(JerseyNumber)
@@ -70,11 +78,11 @@ CREATE  TABLE PLAYEREQ(
     );  
 
 CREATE  TABLE MEDRECORDS(
-	RecordID			Int(3)		NOT NULL,
-    StaffID             Int(3) 		NOT NULL,
-    JerseyNumber        Int(2)     	NOT NULL,
-    InjuryStatus        Char(50)   	NULL,
-    Allergies           Char(50) 	NULL,
+	RecordID			Int(3)			NOT NULL,
+    StaffID             Int(3) 			NOT NULL,
+    PlayerID        	Int(4)     		NOT NULL,
+    InjuryStatus        Char(50)   		NULL,
+    Allergies           Char(50) 		NULL,
     CONSTRAINT          MEDRECORDS_PK     PRIMARY KEY(RecordID),
     CONSTRAINT          MEDREC_PLAYERS_FK        FOREIGN KEY(JerseyNumber)
                             REFERENCES PLAYERS(JerseyNumber)
@@ -87,11 +95,11 @@ CREATE  TABLE MEDRECORDS(
     );
 
 CREATE  TABLE TRAINEREQ(
-    TrainerEQID         Int(3) 		NOT NULL,
-    StaffID        Int(3) 		NULL,
+    TrainerEQID         Int(3) 			NOT NULL,
+    StaffID        		Int(3) 			NULL,
     Description         Char(100)   	NOT NULL,
     Travel              Char(1)        	NOT NULL,
-    ExpectedLife        Int(2) 		NOT NULL,
+    ExpectedLife        Int(2) 			NOT NULL,
     CONSTRAINT          TRAINEREQ_PK    PRIMARY KEY(TrainerEQID),
     CONSTRAINT 		TRAINEREQ_MEDIC_FK 	FOREIGN KEY(StaffID)
     				REFERENCES MEDICALSTAFF(StaffID)
