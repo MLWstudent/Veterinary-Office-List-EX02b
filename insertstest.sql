e97ef944782e790658dc8a30500053efeadbce1b
jcsmith1860377 <55199762+jcsmith1860377@users.noreply.github.com>
2019-12-03 11:47:39 -0600

    Update insertstest.sql

--- insertstest.sql
+++ insertstest.sql
@@ -1,1 +1,307 @@
+INSERT INTO PLAYERS VALUES
+('01', 'Joe', 'Williams', 'QB', '3.50', '2020', '15', '20');
+INSERT INTO PLAYERS VALUES
+('02', 'Tom','Jackson','RB','3.20','2021','10','15');
+INSERT INTO PLAYERS VALUES
+('03', 'Brad', 'Smith', 'WR', '2.74','2022','12','5');
+INSERT INTO PLAYERS VALUES
+('04', 'Steve', 'Jones', 'WR', '2.92', '2021', '11','2');
+INSERT INTO PLAYERS VALUES
+('05', 'Jack', 'Smith', 'OL', '2.21', '2020', '30', '10');
+INSERT INTO PLAYERS VALUES
+('06', 'Joe', 'Jackson', 'OL', '2.75', '2022', '17', '4');
+INSERT INTO PLAYERS VALUES
+('07', 'Andrew', 'Brady', 'OL', '3.50', '2021', '3', '15');
+INSERT INTO PLAYERS VALUES
+('08', 'Tim', 'Johnson', 'OL', '3.20', '2022', '5','12');
+INSERT INTO PLAYERS VALUES
+('09', 'Daniel', 'Moss', 'OL', '2.85', '2023', '10', '20');
+INSERT INTO PLAYERS VALUES
+('10', 'Anthony', 'Harrison', 'TE', '3.90', '2021', '12', '25');
+INSERT INTO PLAYERS VALUES
+('11', 'Rob', 'Eifert', 'TE', '3.21', '2021', '18', '11');
+INSERT INTO PLAYERS VALUES
+('12', 'Sam', 'Robinson', 'DL', '2.46', '2023', '2', '21');
+INSERT INTO PLAYERS VALUES
+('13', 'Jared', 'Long', 'DL', '2.19', '2020', '3', '14');
+INSERT INTO PLAYERS VALUES
+('14', 'Phillip', 'Johnson', 'DL', '2.81', '2023', '5', '10');
+INSERT INTO PLAYERS VALUES
+('15', 'Julian', 'Schuster', 'LB', '3.00', '2022', '10', '19');
+INSERT INTO PLAYERS VALUES
+('16', 'Justin', 'Rogers', 'LB', '2.90', '2021', '8', '13');
+INSERT INTO PLAYERS VALUES
+('17', 'Max', 'Karras', 'LB', '3.20', '2022', '1', '12');
+INSERT INTO PLAYERS VALUES
+('18', 'Jermaine', 'Bettis', 'LB', '3.40', '2021', '4', '20');
+INSERT INTO PLAYERS VALUES
+('19', 'Matthew', 'McCalister', 'CB', '3.20', '2020', '10', '25');
+INSERT INTO PLAYERS VALUES
+('20', 'Josh', 'Lockett', 'CB', '3.10', '2022', '20', '10');
+INSERT INTO PLAYERS VALUES
+('21', 'Gardner', 'Portanova', 'S', '3.20', '2020', '30', '24');
+INSERT INTO PLAYERS VALUES
+('22', 'Devin', 'Ryan', 'S', '2.71', '2023', '21', '12');
+INSERT INTO PLAYERS VALUES
+('23', 'Justin', 'Stevens', 'ST', '2.80', '2021', '26', '2');
+INSERT INTO PLAYERS VALUES
+('24', 'Patrick', 'Donahue', 'ST', '3.20', '2022', '14', '5');
+INSERT INTO PLAYERS VALUES
+('25', 'Aaron', 'Goff', 'ST', '3.62', '2021', '17', '10');
 
+
+
+INSERT INTO COACHES VALUES
+('001', 'Robert', 'Perkins', 'Offense', 'Offense Coordinator', 'Y');
+INSERT INTO COACHES VALUES
+('002', 'Tom', 'Johnson', 'Defense', 'Defensive Coordinator', 'Y');
+INSERT INTO COACHES VALUES
+('003', 'Nathan', 'Howard', 'Offense', 'Head Coach', 'Y');
+INSERT INTO COACHES VALUES
+('004', 'Mike', 'Wagner', 'OL', 'Offensive Line', 'Y');
+INSERT INTO COACHES VALUES
+('005', 'Tasha', 'Ortiz', 'S', 'Safety Coach', 'Y');
+INSERT INTO COACHES VALUES
+('006', 'John', 'Ramirez', 'DL', 'Defensive Line', 'Y');
+INSERT INTO COACHES VALUES
+('007', 'Peter', 'Bones', 'LB', 'Lineback Coach', 'Y');
+INSERT INTO COACHES VALUES
+('008', 'Samuel', 'Moore', 'QB', 'Quarterback Coach', 'Y');
+INSERT INTO COACHES VALUES
+('009', 'Joseph', 'Liberty', 'WR', 'Recieving Coach', 'Y');
+INSERT INTO COACHES VALUES
+('010', 'Paul', 'Owens', 'RB', 'Runningback Coach', 'Y');
+INSERT INTO COACHES VALUES
+('011', 'Simmons', 'Dounut', 'TE', 'Tight End Coach', 'Y');
+INSERT INTO COACHES VALUES
+('012', 'Fred', 'Bell', 'ST', 'Special Teams Coach', 'Y');
+INSERT INTO COACHES VALUES
+('013', 'Steve', 'East', 'ST', 'Kicking Coach', 'Y');
+INSERT INTO COACHES VALUES
+('014', 'Charles', 'Murray', 'DL', 'Assistant Defensive Line', 'Y');
+INSERT INTO COACHES VALUES
+('015', 'James', 'Richardson', 'OL', 'Assistant Offensive Line', 'Y');
+INSERT INTO COACHES VALUES
+('116', 'David', 'Williams', 'DL', 'Grad Assistant', 'N');
+INSERT INTO COACHES VALUES
+('117', 'John', 'Ryans', 'OL', 'Grad Assistant', 'N');
+INSERT INTO COACHES VALUES
+('118', 'Will', 'Barnes', 'ST', 'Grad Assistant', 'N');
+INSERT INTO COACHES VALUES
+('119', 'Andrew', 'Young', 'WR', 'Grad Assistant', 'N');
+INSERT INTO COACHES VALUES
+('120', 'Jimmy', 'Oak', 'CB', 'Grad Assistant', 'N');
+
+
+
+INSERT INTO HIGHSCHOOL VALUES
+('001', 'Evansville High School', '455 5th St', 'Evansville', 'IN', '55555', '68');
+INSERT INTO HIGHSCHOOL VALUES
+('002', 'DeKalb High School', '375 Main St', 'DeKalb', 'IL', '60115', '1');
+INSERT INTO HIGHSCHOOL VALUES
+('003', 'West Aurora High School', '1201 W New York St', 'Aurora', 'IL', '60506', '03');
+INSERT INTO HIGHSCHOOL VALUES
+('004', 'Batavia High School', '450 Wilson St', 'Batavia', 'IL', '60510', '40');
+INSERT INTO HIGHSCHOOL VALUES
+('005', 'Milwaukee High School', '1200 E 35th St', 'Milwaukee', 'WI', '53205', '110');
+INSERT INTO HIGHSCHOOL VALUES
+('006', 'Bartlett High School', '575 W IL Rt. 59', 'Bartlett', 'IL', '60103', '55');
+INSERT INTO HIGHSCHOOL VALUES
+('007', 'Saint Charles North High School', '2400 Kirk Rd', 'Saint Charles', 'IL', '60175', '45');
+INSERT INTO HIGHSCHOOL VALUES
+('008', 'Indianapolis South High School', '400 E Downers St', 'Indianapolis', 'IN', '46077', '140');
+INSERT INTO HIGHSCHOOL VALUES
+('009', 'Marmion Academy', '500 W IL Rt. 56', 'Aurora', 'IL', '60502', '40');
+INSERT INTO HIGHSCHOOL VALUES
+('010', 'Oshkosh High School', '1200 W Stephens St.', 'Oshkosh', 'WI', '54901', '80');
+INSERT INTO HIGHSCHOOL VALUES
+('011', 'Green Bay High School', '1700 E 156th St.', 'Green Bay', 'WI', '54229', '120');
+INSERT INTO HIGHSCHOOL VALUES 
+('012', 'Crystal Lake High School', '500 Randall Rd.', 'Crystal Lake', 'IL', '60012', '70');
+INSERT INTO HIGHSCHOOL VALUES
+('013', 'Los Angeles West High School', '490 W 127th St.', 'Los Angeles', 'CA', '90011', '2700');
+INSERT INTO HIGHSCHOOL VALUES
+('014', 'Champaign High School', '360 Mckee St', 'Champaign', 'IL', '61820', '170');
+INSERT INTO HIGHSCHOOL VALUES
+('015', 'Pittsburgh South High School', '1200 E 5th St.', 'Pittsburgh', 'PA', '15213', '810');
+INSERT INTO HIGHSCHOOL VALUES
+('016', 'South High School', '90 1st St.', 'Iowa City','IA', '52242', '200');
+INSERT INTO HIGHSCHOOL VALUES
+('017', 'Minneapolis Academy', '570 W Hennepin Ave.', 'Minneapolis', 'MN', '55401', '350');
+INSERT INTO HIGHSCHOOL VALUES
+('018', 'Kaneland High School', '50 W IL Rt. 38', 'Elburn', 'IL', '60119', '10');
+INSERT INTO HIGHSCHOOL VALUES
+('019', 'Waubonsee Valley High School', '750 Eola St.', 'Aurora', 'IL', '60502', '20');
+INSERT INTO HIGHSCHOOL VALUES
+('020', 'Naperville Central High School', '350 S Washington St.', 'Naperville', 'IL', '60563', '27');
+
+
+
+INSERT INTO MEDICALSTAFF VALUES
+('201', 'Alan', 'Stewart','Head Doctor', 'Y'),
+('202', 'Henry', 'Matthews','Head Trainer', 'Y'),
+('203', 'Royce', 'Phillips', 'Physical Therapist', 'Y'),
+('204', 'Alonso', 'Davis', 'Physical Therapist', 'Y'),
+('205', 'Arthur', 'Chavez', 'Physical Therapist', 'N'),
+('206', 'Samuel', 'Walker', 'Physical Therapist', 'N'),
+('207', 'Rocco', 'Thomas', 'Athletic Trainer', 'Y'),
+('208', 'Cole', 'Warren', 'Athletic Trainer', 'Y'),
+('209', 'Ajay', 'Gibson', 'Athletic Trainer', 'N'),
+('210', 'Maya', 'Stone', 'Athletic Trainer', 'N'),
+('211', 'Leroy', 'Barnes', 'Exercise Physiologist', 'Y'),
+('212', 'Brenda', 'Burns', 'Exercise Physiologist', 'Y'),
+('213', 'Shane', 'Thompson', 'Exercise Physiologist', 'N'),
+('214', 'Lucas', 'Fox', 'Exercise Physiologist', 'N'),
+('215', 'Reon', 'Hayward', 'Kinesiotherapist', 'Y'),
+('216', 'Yvonne', 'Frank', 'Kinesiotherapist', 'Y'),
+('217', 'Davey', 'Mays', 'Kinesiotherapist', 'N'),
+('218', 'Don', 'Mathis', 'Kinesiotherapist', 'N'),
+('219', 'Trystan', 'Richmond', 'Assissant Doctor', 'Y'),
+('220', 'Liana', 'Rahman', 'Assissant Doctor', 'N'),
+('221', 'Valarie', 'Barajas', 'Assissant Trainer', 'Y'),
+('222', 'Jadon', 'Pickett', 'Assissant Trainer', 'N');
+
+
+
+INSERT INTO RECRUITS VALUES
+('400', '001', 'Jake', 'Gonzalez', 'WR', '2021');
+INSERT INTO RECRUITS VALUES
+('401', '003', 'Cameron', 'Paul', 'TE', '2021');
+INSERT INTO RECRUITS VALUES
+('402', '002', 'Nick', 'Ryan', 'RB', '2021');
+INSERT INTO RECRUITS VALUES
+('403', '005', 'Logan', 'Williams', 'QB', '2021');
+INSERT INTO RECRUITS VALUES
+('404', '001', 'Austin', 'Jackson', 'WR', '2021');
+INSERT INTO RECRUITS VALUES
+('405', '010', 'Cameron', 'Jackson', 'OL', '2021');
+INSERT INTO RECRUITS VALUES
+('406', '011', 'Paul', 'Crayne', 'Kicker', '2021');
+INSERT INTO RECRUITS VALUES
+('407', '014', 'Ryan', 'Rivera', 'DE', '2021');
+INSERT INTO RECRUITS VALUES
+('408', '017', 'Justin', 'Gonzalez', 'DE', '2022');
+INSERT INTO RECRUITS VALUES
+('409', '002', 'Johan', 'Vivar', 'OL', '2022');
+INSERT INTO RECRUITS VALUES
+('410', '006', 'Diego', 'Rodriguez', 'QB', '2022');
+INSERT INTO RECRUITS VALUES
+('411', '020', 'John', 'Smith', 'DB', '2022');
+INSERT INTO RECRUITS VALUES
+('412', '007', 'Connor', 'Ryan', 'DB', '2022');
+INSERT INTO RECRUITS VALUES
+('413', '008', 'Jake', 'Elliot', 'RB', '2022');
+INSERT INTO RECRUITS VALUES
+('414', '015', 'Micheal', 'Smith', 'DB', '2022');
+INSERT INTO RECRUITS VALUES
+('415', '001', 'Ryan', 'Delgado', 'TE', '2023');
+INSERT INTO RECRUITS VALUES
+('416', '003', 'Kevin', 'Gonzalez', 'OL', '2023');
+INSERT INTO RECRUITS VALUES
+('417', '004', 'Jarod', 'Johnson', 'DT', '2023');
+INSERT INTO RECRUITS VALUES
+('418', '009', 'Skylar', 'Williams', 'DT', '2023');
+INSERT INTO RECRUITS VALUES
+('419', '012', 'Demarious', 'Randall', 'DB', '2023');
+INSERT INTO RECRUITS VALUES
+('420', '016', 'Julio', 'Jones', 'WR', '2023');
+INSERT INTO RECRUITS VALUES
+('421', '018', 'Patrick', 'Mahomes', 'QB', '2024');
+
+
+INSERT INTO PLAYEREQ VALUES
+('301', '01', 'Helmet', 'Y',  '5'); 
+INSERT INTO PLAYEREQ VALUES
+('303', '02', 'Jersey', 'Y',  '2');
+INSERT INTO PLAYEREQ VALUES
+('305', '03', 'Mouthguard', 'N',  '1');
+INSERT INTO PLAYEREQ VALUES
+('302', '04', 'Helmet', 'Y', '5');
+INSERT INTO PLAYEREQ VALUES
+('304', '06', 'Jersey', 'Y',  '2');
+INSERT INTO PLAYEREQ VALUES
+('307', '05', 'Mouthguard', 'N',  '5');
+INSERT INTO PLAYEREQ VALUES
+('309', '08', 'Mouthguard', 'N',  '1');
+INSERT INTO PLAYEREQ VALUES
+('308', '09', 'ShoulderPads', 'Y',  '3'); 
+INSERT INTO PLAYEREQ VALUES
+('312', '10', 'ShoulderPads', 'N', '3');
+INSERT INTO PLAYEREQ VALUES
+('314', '13', 'Jersey', 'Y', '2');
+INSERT INTO PLAYEREQ VALUES
+('313', '15', 'KneePad', 'Y', '4'); 
+INSERT INTO PLAYEREQ VALUES
+('315', '17', 'Mouthguard', 'N',  '1');
+INSERT INTO PLAYEREQ VALUES
+('319', '14', 'KneePad', 'Y',  '4');
+INSERT INTO PLAYEREQ VALUES
+('320', '16', 'Helmet', 'Y',  '5');
+INSERT INTO PLAYEREQ VALUES
+('321', '18', 'KneePad', 'Y',  '4');
+INSERT INTO PLAYEREQ VALUES
+('323', '19', 'Jersey', 'Y',  '2');
+INSERT INTO PLAYEREQ VALUES
+('324', '20', 'ShoulderPad', 'Y',  '5');
+INSERT INTO PLAYEREQ VALUES
+('325', '21', 'Jersey', 'Y',  '2');
+INSERT INTO PLAYEREQ VALUES
+('327', '22', 'ShoulderPad', 'Y',  '4');
+INSERT INTO PLAYEREQ VALUES
+('326', '23', 'Helmet', 'Y',  '5');
+INSERT INTO PLAYEREQ VALUES
+('330', '24', 'KneePad', 'Y',  '4');
+INSERT INTO PLAYEREQ VALUES
+('328', '25', 'Jersey', 'Y',  '2');
+
+
+
+INSERT INTO MEDRECORDS (RecordID, StaffID, JerseyNumber, Allergies) VALUES
+('01', '201', '03', 'Peanuts'),
+('02', '219', '04', 'Fish'),
+('03', '201', '04', 'Milk'),
+('04', '201', '06', 'Peanuts'),
+('05', '220', '09', 'Hot Peppers'),
+('06', '201', '10', 'Milk'),
+('07', '201', '15', 'Soy'),
+('08', '201', '18', 'Peanuts'),
+('09', '201', '18', 'Tree Nut'),
+('10', '201', '21', 'Wheat'),
+('11', '201', '21', 'Tree Nut'),
+('12', '201', '23', 'Peanuts'),
+('13', '201', '24', 'Peanuts'),
+('14', '201', '24', 'Tree Nut'),
+('15', '220', '25', 'Shellfish');
+
+INSERT INTO MEDRECORDS (RecordID, StaffID, JerseyNumber, InjuryStatus) VALUES
+('51', '211', '07', 'Sprained Hand'),
+('52', '212', '09', 'Wrist Sprain'),
+('53', '206', '18', 'Concussion'),
+('54', '213', '20', 'Sprained leg'),
+('55', '215', '13', 'Lower Back Pains');
+
+
+
+INSERT INTO TRAINEREQ VALUES
+('151', '201', 'Wrap', 'Y', '1'),
+('152', '219', 'Wrap', 'Y', '1'),
+('153', '220', 'Wrap', 'Y', '1'),
+('154', '208', 'Wrap', 'Y', '1'),
+('155', '210', 'Wrap', 'Y', '1'),
+('156', '221', 'Brace', 'Y', '3'),
+('157', '221', 'Brace', 'Y', '3'),
+('158', '222', 'Brace', 'Y', '2'),
+('159', '222', 'Brace', 'Y', '3'),
+('160', '219', 'Band Aids', 'Y', '1'),
+('161', '219', 'Band Aids', 'Y', '1'),
+('162', '219', 'Band Aids', 'Y', '1'),
+('163', '220', 'Band Aids', 'Y', '1'),
+('164', '220', 'Band Aids', 'Y', '1'),
+('165', '201', 'Band Aids', 'Y', '1'),
+('166', '201', 'Band Aids', 'Y', '1'),
+('170', '216', 'Resistance Band', 'Y', '5');
+
+INSERT INTO TRAINEREQ (TrainerEQID, Description, Travel, ExpectedLife) VALUES
+('167', 'Hydrotherapy Tub', 'N', '8'),
+('168', 'Agility Ladder', 'Y', '8'),
+('169', 'Agility Ladder', 'N', '8');
